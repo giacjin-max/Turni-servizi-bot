57f70a503e117cea2620454762bdda7904042b6b
giacjin-max <giac.jin@gmail.com>
2026-06-14 14:49:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -185,16 +185,6 @@ def webhook():
 def home():
     return "OK", 200
 
-# =======
-# test
-# =======
-@app.route("/test-sender")
-def test_sender():
-    import os
-    os.system("python sender.py")
-    return "sender eseguito", 200
-    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    

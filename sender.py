7a97ef3ed4e9764c79a31fc51fbf5f1f18bbb563
giacjin-max <giac.jin@gmail.com>
2026-06-15 19:56:59 +0200

    Update sender.py

--- sender.py
+++ sender.py
@@ -234,15 +234,3 @@ def reminder_sabato():
 
     print("📢 Reminder sabato inviato")
 
-# =====================
-# SCHEDULER
-# =====================
-scheduler = BackgroundScheduler()
-
-scheduler.add_job(send, "cron", day_of_week="mon", hour=9, minute=0)
-scheduler.add_job(run_reminder, "cron", day_of_week="thu", hour=9, minute=0)
-scheduler.add_job(reminder_sabato, "cron", day_of_week="sat", hour=10, minute=0)
-
-scheduler.start()
-
-print("🚀 BOT ATTIVO")
